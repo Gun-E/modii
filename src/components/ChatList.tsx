c3c06f9e188c3926f4fd63cc5a660a772c767a2b
gun <rkdrjs71@naver.com>
2024-09-19 12:21:28 +0900

    feat : 그룹톡 추가

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -2,61 +2,56 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import React, {useState, useEffect} from "react";
-import {ChatListDTO} from "@/types/ChatListDTO";
+import React, { useState, useEffect } from "react";
+import { ChatListDTO } from "@/types/ChatListDTO";
 import { formatDate } from '@/utils/utils';
+import GroupChat from "@/components/GroupChat";
+
+const CACHE_KEY = 'chatListCache';
+const CACHE_EXPIRY_TIME = 5 * 60 * 1000; // 5 minutes in milliseconds
 
 export default function ChatList() {
     const [chatList, setChatList] = useState<ChatListDTO[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        fetch('https://aqueous-coast-82122-c626a44767e1.herokuapp.com/device/1/chat-list')
-            .then((response) => response.json())
-            .then((data: ChatListDTO[]) => {
+        const fetchData = async () => {
+            const now = Date.now();
+            const cachedData = localStorage.getItem(CACHE_KEY);
+
+            if (cachedData) {
+                const { timestamp, data } = JSON.parse(cachedData);
+                if (now - timestamp < CACHE_EXPIRY_TIME) {
+                    setChatList(data);
+                    setIsLoading(false);
+                    return;
+                }
+            }
+
+            try {
+                const response = await fetch('https://aqueous-coast-82122-c626a44767e1.herokuapp.com/device/1/chat-list');
+                const data: ChatListDTO[] = await response.json();
+                localStorage.setItem(CACHE_KEY, JSON.stringify({ timestamp: now, data }));
                 setChatList(data);
-            })
-            .catch((error) => {
+            } catch (error) {
                 console.error('Error fetching API data:', error);
-            });
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
     }, []);
 
+    if (isLoading) {
+        return <div className="loading-overlay"><div className="loading-spinner"></div></div>;
+    }
 
     return (
         <div className="chat-list">
             <Link href={`/`} className="chat-body">
                 <div className="chat-content">
-                    <div className="double-circle">
-                        <div className="circle-chat absolute top-0 start-0">
-                            <Image
-                                src={`/images/robotvacuum.png`}
-                                alt={`refrigerator`}
-                                style={{
-                                    objectFit: 'cover',
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                    backgroundColor: '#f8afc6'
-                                }}
-                                width={42}
-                                height={42}
-                            />
-
-                        </div>
-                        <div className="circle-chat absolute bottom-0 end-0">
-                            <Image
-                                src={`/images/refrigerator.png`}
-                                alt={`refrigerator`}
-                                style={{
-                                    objectFit: 'cover',
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                    backgroundColor: '#D6FCD8'
-                                }}
-                                width={42}
-                                height={42}
-                            />
-                        </div>
-                    </div>
-
+                    <GroupChat/>
                     <div className="chat-text">
                         <h2 className="modii-name">울집 애기들</h2>
                         <div className="message-time">
@@ -69,8 +64,8 @@ export default function ChatList() {
             {chatList.map((data) => (
                 <Link href={`/talk/${data.deviceId}`} key={data.deviceId} className="chat-body">
                     <div className="chat-content">
-                        <div className="circle-chat" style={{backgroundColor: data.image}}>
-                        <Image
+                        <div className="circle-chat" style={{ backgroundColor: data.image }}>
+                            <Image
                                 src={`/images/${data.deviceType}.png`}
                                 alt={data.name}
                                 style={{
--- src/components/GroupChat.tsx
+++ src/components/GroupChat.tsx
@@ -2,72 +2,41 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
-import { formatDate } from '@/utils/utils';
 
-interface GroupChatProps {
-    href: string;
-    robotVacuumImage: string;
-    refrigeratorImage: string;
-    robotVacuumBgColor: string;
-    refrigeratorBgColor: string;
-    name: string;
-    message: string;
-    messageDate: string;
-}
 
-const GroupChat: React.FC<GroupChatProps> = ({
-                                               href,
-                                               robotVacuumImage,
-                                               refrigeratorImage,
-                                               robotVacuumBgColor,
-                                               refrigeratorBgColor,
-                                               name,
-                                               message,
-                                               messageDate
-                                           }) => {
+const GroupChat = () => {
     return (
-        <Link href={href} className="chat-body">
-            <div className="chat-content">
-                <div className="double-circle">
-                    <div className="circle-chat absolute top-0 start-0">
-                        <Image
-                            src={robotVacuumImage}
-                            alt="robot vacuum"
-                            style={{
-                                objectFit: 'cover',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                backgroundColor: robotVacuumBgColor
-                            }}
-                            width={42}
-                            height={42}
-                        />
-                    </div>
-                    <div className="circle-chat absolute bottom-0 end-0">
-                        <Image
-                            src={refrigeratorImage}
-                            alt="refrigerator"
-                            style={{
-                                objectFit: 'cover',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                backgroundColor: refrigeratorBgColor
-                            }}
-                            width={42}
-                            height={42}
-                        />
-                    </div>
-                </div>
+        <div className="double-circle">
+            <div className="circle-chat absolute top-0 start-0">
+                <Image
+                    src={`/images/robotvacuum.png`}
+                    alt={`refrigerator`}
+                    style={{
+                        objectFit: 'cover',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor: '#f8afc6'
+                    }}
+                    width={42}
+                    height={42}
+                />
 
-                <div className="chat-text">
-                    <h2 className="modii-name">{name}</h2>
-                    <div className="message-time">
-                        <span className="message">{message}</span>
-                        <span className="time">{formatDate(messageDate)}</span>
-                    </div>
-                </div>
             </div>
-        </Link>
+            <div className="circle-chat absolute bottom-0 end-0">
+                <Image
+                    src={`/images/refrigerator.png`}
+                    alt={`refrigerator`}
+                    style={{
+                        objectFit: 'cover',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor: '#D6FCD8'
+                    }}
+                    width={42}
+                    height={42}
+                />
+            </div>
+        </div>
     );
 };
 
